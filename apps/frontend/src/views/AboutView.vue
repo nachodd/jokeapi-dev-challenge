c137ba9058edd476f6d3972eaa111d11ae4f6512
Ignacio Durand <ignacio.durand@l7informatics.com>
2024-12-18 13:34:54 -0300

    Added content to the About page

--- apps/frontend/src/views/AboutView.vue
+++ apps/frontend/src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="max-w-4xl mx-auto p-6">
+    <h1 class="text-2xl font-bold mb-4">About This Project</h1>
+    <p>👋 Hi! I'm Ignacio Durand, a fullstack developer with a focus on the Frontend.</p>
+    <p>🚀 This small project is part of a developer challenge for a frontend position.</p>
+    <p>🛠️ It was built using Vue.js and shadcn-vue for components and styling. The project is organized as a monorepo using Turbo.</p>
+    <p>🔧 The backend is based on the <a class="hover:underline" href="https://github.com/15Dkatz/official_joke_api/" target="_blank">Official Joke Api</a>. It uses Express and limiting-middleware for handling requests and rate limiting.</p>
+    <p class="mb-2">📦 The tools and dependencies used in this project include:</p>
+    <ul class="list-disc list-inside ml-3">
+      <li>Frontend: Vue.js, shadcn-vue, Vite, TailwindCSS</li>
+      <li>Backend: Node, Express, limiting-middleware, Nodemon</li>
+      <li>Monorepo management: Turbo</li>
+    </ul>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
