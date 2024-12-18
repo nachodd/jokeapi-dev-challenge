299bfa484f6ff4e65a48fa93fa1a6af0b94f707d
Ignacio Durand <ignacio.durand@l7informatics.com>
2024-12-18 13:06:39 -0300

    Implement Sort Order on the HomeView

---
+++ apps/frontend/src/components/SortArrow.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { MoveUpIcon, MoveDownIcon } from 'lucide-vue-next'
+import type { SortOrder } from '@/types/jokeTypes'
+const { sortOrder } = defineProps<{
+  sortOrder: SortOrder
+}>()
+</script>
+<template>
+  <template v-if="sortOrder==='asc'"><MoveUpIcon class="h-3 w-3"/></template>
+  <template v-else><MoveDownIcon class="h-3 w-3"/></template>
+</template>
--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -19,20 +19,40 @@ import {
   PaginationNext,
   PaginationPrev,
 } from '@/components/ui/pagination'
+import SortArrow from '@/components/SortArrow.vue'
 
 import { ref, onMounted } from 'vue'
 import JokeService from '@/services/JokeService'
-import type { Joke } from '@/types/jokeTypes'
+import type { Joke, SortOrder } from '@/types/jokeTypes'
 
 const jokes = ref<Joke[]>([])
 const totalJokes = ref(0)
 const currentPage = ref(1)
+const sortBy = ref<string>('')
+const sortOrder = ref<SortOrder>('')
 
 const fetchJokes = async (page: number) => {
-  const { jokes: jokesResponse, total } = await JokeService.getPaginatedJokes((page - 1) * 10, 10)
+  const { jokes: jokesResponse, total } = await JokeService.getPaginatedJokes((page - 1) * 10, 10, sortBy.value, sortOrder.value)
   jokes.value = jokesResponse
   totalJokes.value = total
   currentPage.value = page
+}
+
+const toggleSort = (field: string) => {
+  if (sortBy.value === field) {
+    if (sortOrder.value === 'asc') {
+      sortOrder.value = 'desc'
+    } else if (sortOrder.value === 'desc') {
+      sortOrder.value = ''
+      sortBy.value = ''
+    } else {
+      sortOrder.value = 'asc'
+    }
+  } else {
+    sortBy.value = field
+    sortOrder.value = 'asc'
+  }
+  fetchJokes(currentPage.value)
 }
 
 onMounted(() => {
@@ -48,10 +68,18 @@ onMounted(() => {
         <TableCaption>List of Jokes</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>Type</TableHead>
-            <TableHead>Setup</TableHead>
-            <TableHead>Punchline</TableHead>
+            <TableHead @click="toggleSort('id')" :class="{ 'font-bold': sortBy === 'id' }">
+              ID <span class="inline-block" v-if="sortBy === 'id'"><SortArrow :sortOrder="sortOrder" /></span>
+            </TableHead>
+            <TableHead @click="toggleSort('type')" :class="{ 'font-bold': sortBy === 'type' }">
+              Type <span class="inline-block" v-if="sortBy === 'type'"><SortArrow :sortOrder="sortOrder" /></span>
+            </TableHead>
+            <TableHead @click="toggleSort('setup')" :class="{ 'font-bold': sortBy === 'setup' }">
+              Setup <span class="inline-block" v-if="sortBy === 'setup'"><SortArrow :sortOrder="sortOrder" /></span>
+            </TableHead>
+            <TableHead @click="toggleSort('punchline')" :class="{ 'font-bold': sortBy === 'punchline' }">
+              Punchline <span class="inline-block" v-if="sortBy === 'punchline'"><SortArrow :sortOrder="sortOrder" /></span>
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
