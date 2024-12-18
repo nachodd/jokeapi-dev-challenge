f754bc8f4eecd71a9d57dc643e8944bb49aad8d0
Ignacio Durand <ignacio.durand@l7informatics.com>
2024-12-18 11:05:00 -0300

    Remove old code from App

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,16 +1,6 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import JokeService from './services/JokeService'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
-
-const randomJoke = ref('')
-
-onMounted(async () => {
-  randomJoke.value = await JokeService.getRandomJoke()
-})
-
 </script>
 
 <template>
@@ -18,12 +8,3 @@ onMounted(async () => {
 
   <RouterView />
 </template>
-
-<style scoped>
-.dot {
-  transition: transform 0.3s ease-in-out;
-}
-input:checked + .dot {
-  transform: translateX(100%);
-}
-</style>
